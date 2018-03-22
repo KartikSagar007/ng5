f4416b492a629b4da7230f6b3d1b7988e17df1dc
Bagga <sagar.bagga@capgemini.com>
2018-03-22 19:59:58 +0530

    "Bootstrap added in Angular 5"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { DemoComponent } from './demo/demo.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'demo',
+    component: DemoComponent
+  },
+  {
+    path: '**',
+    component: NotfoundComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotfoundComponent,
+    DemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css']
+})
+export class DemoComponent implements OnInit {
+  usable: boolean = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  toggleAnchor() {
+    this.usable = !this.usable;
+    console.log(this.usable);
+  }
+  doOperation(e) {
+    if (this.usable !== true) {
+      // e.preventDefault();
+    }
+    else {
+      alert("Anchor element is enabled");
+    }
+  }
+}
